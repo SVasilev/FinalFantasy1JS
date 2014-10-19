79141a217a12364de7ed671b76a6689db132bb82
SVasilev <thesopa@abv.bg>
2014-10-19 17:59:05 +0300

    Add CreteJS game engine. Add Node js express module. Add server.js which you can run to use your site and services on port 8080. Add small index.html example with CreateJS.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('./lib/node_modules/express'),
+	app     = express(),
+	port    = process.env.PORT || 8080;
+
+//Queries to database are made through http://localhost:8080/services/<backendFile>.js
+app.get(/.*services\/(.+)\.js.*/, function(req, res) {
+	var servicePath = './services/' + req.params[0] + '.js';
+	service = require(servicePath);
+	res.end(JSON.stringify(req.query));
+	//res.end(servicePath);
+	//res.end("asd");
+});
+
+app.use(express.static(__dirname + '/web'));
+app.use(express.static(__dirname + '/'));
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
---
+++ services/gameQueries.js
@@ -0,0 +1,3 @@
+function asd() {
+	return 42;
+}
