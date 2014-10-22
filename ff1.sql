3caeb6b4bd15b8087fa9ce2f03de17f83649280e
SVasilev <thesopa@abv.bg>
2014-10-22 21:49:46 +0300

    Pre-commit and post-merge work properly. Add new game library: phaser. Should consider which one to use. Fix login exception text.

--- ff1.sql
+++ ff1.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: ff1
 -- ------------------------------------------------------
--- Server version   5.6.21-log
+-- Server version	5.6.21-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,25 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `lololo`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `lololo`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lololo` (
-  `ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `salt` varchar(12) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lololo`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `lololo` WRITE;
-/*!40000 ALTER TABLE `lololo` DISABLE KEYS */;
-INSERT INTO `lololo` VALUES (24);
-/*!40000 ALTER TABLE `lololo` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'thesopa@abv.bg','asd','asd','2014-10-22 21:41:09');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-21 23:51:07
+-- Dump completed on 2014-10-22 21:49:46
