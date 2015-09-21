4345eb3b17e5cbb208278aa48e58f611b1daeb48
SVasilev <thesopa@abv.bg>
2015-09-21 16:07:54 +0300

    Big refactoring in repo structure and code.

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-var express = require('./lib/node_modules/express'),
-	app     = express(),
-	port    = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 
-//Queries to database are made through http://localhost:8080/services/<backendFile>.js
-app.get(/.*services\/(.+)\.js.*/, function(request, response) {
-	var servicePath = './services/' + request.params[0] + '.js';
-	var service = require(servicePath);
-	//alert(request.query.cmd);
-	//response.end(JSON.stringify(request.query));
-	service.executeCommand(request, response);
-});
+var loginService = require('./lib/services/login/login');
+
+app.get('/services/login', loginService);
 
 app.use(express.static(__dirname + '/web'));
 app.use(express.static(__dirname + '/'));
---
+++ web/ui/loginUtils.js
@@ -0,0 +1,51 @@
+var loginUtils = {
+	fadeOut: function(elementID, newText) {
+		var paragraph = document.getElementById(elementID);
+		var opacity = 1;
+
+		if (paragraph.innerHTML.length < 10 || elementID === 'login') {
+			if (elementID !== 'login') {
+				paragraph.innerHTML = newText;
+			}
+			var timer = setInterval(function() {
+				if (opacity <= 0.1) {
+					clearInterval(timer);
+					if (newText === 'Logged in.') {
+						presentationUtils.fadeOut('login', 'Logging in.');
+					}
+					else {
+						paragraph.innerHTML = '&nbsp;';
+					}
+				}
+				paragraph.style.opacity = opacity.toString();
+				paragraph.style.filter = 'alpha(opacity=' + opacity * 100 + ')';
+				opacity -= opacity * 0.1;
+			}, 80);
+		}
+	},
+
+	executeQuery: function(parameters) {
+		$.ajax({
+			url: '/services/login?' + parameters,
+			success: function(response) {
+				presentationUtils.fadeOut('exceptionText', response.toString());
+			},
+			error: function(response) {
+				alert(response.responseText);
+			}
+		});
+	},
+
+	loginOrRegister: function() {
+		var email = document.getElementById('email')['value'];
+		var password = document.getElementById('password')['value'];
+		var request = 'email=' + email + '&password=' + password;
+
+		if (email && password) {
+			this.executeQuery(request);
+		}
+		else {
+			this.fadeOut('exceptionText', 'Please fill the fields.');
+		}
+	}
+};
